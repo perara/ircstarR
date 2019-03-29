06c51661f13c8d66b4a4ddf27b772199ed6755b2
einar larsen <einarlarsen@outlook.com>
2019-03-29 21:46:03 +0100

    working dump

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,7 @@ config_file = "config.json"
 
 if not os.path.isfile(config_file):
     # For testing purposes
-    config_file = "example_config.json"
+    config_file = "../config.json"
 
 with open(config_file, "r") as cfg:
     config = json.load(cfg)
